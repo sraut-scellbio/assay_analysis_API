faddaa38747f22764538f1de32ac9aa753133abc
Shiska Raut <sraut@scellbio.com>
2024-08-20 11:49:28 -0500

    first commit

---
+++ clono_analysis/clono_assay/clono_assay.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all radio buttons with name 'option'
+  const radios = document.querySelectorAll('input[name="analysis_type"]');
+  var nwellsDn = document.getElementById('num_wells');
+
+  // when the number of wells is toggled
+  nwellsDn.addEventListener('change', function() {
+    // disable everything
+   document.querySelectorAll('.image-inputs input').forEach(function(input){
+     input.disabled = true
+   var inputs1 = document.querySelectorAll('.d-1 input')
+   var inputsn = document.querySelectorAll('.d-n input')
+   var numWells = document.getElementById('num_wells').value;
+   if (document.getElementById('single_day').checked) {
+     for (var i = 0; i < 2*numWells; i++) {
+       inputs1[i].disabled = false
+       inputsn[i].disabled = true
+     }
+   }
+   else if (document.getElementById('multi_day').checked) {
+     for (var i = 0; i < 2*numWells; i++) {
+       inputs1[i].disabled = false
+       inputsn[i].disabled = false
+     }
+   }
+  });
+ });
+ radios.forEach(function(radio) {
+   radio.addEventListener('change', function() {
+     var inputs1 = document.querySelectorAll('.d-1 input')
+     var inputsn = document.querySelectorAll('.d-n input')
+     var numWells = document.getElementById('num_wells').value;
+     if (document.getElementById('single_day').checked) {
+       for (var i = 0; i < 2*numWells; i++) {
+         inputs1[i].disabled = false
+         inputsn[i].disabled = true
+       }
+     }
+     else if (document.getElementById('multi_day').checked) {
+       for (var i = 0; i < 2*numWells; i++) {
+         inputs1[i].disabled = false
+         inputsn[i].disabled = false
+       }
+     }
+   });
+ });
+});
+
+// if the form is submitted
+document.querySelectorAll('input').eq(-1).onclick(function() {
+  // save user inputs and send to backend
+});
---
+++ scellbio_landing.js
@@ -0,0 +1,7 @@
+document.getElementById('assay_select').addEventListener('click', function() {
+        // Get the selected value from the dropdown
+        var selectedPage = document.getElementById('assaytype').value;
+
+        // Redirect the user to the selected page
+        window.location.href = selectedPage;
+    });
