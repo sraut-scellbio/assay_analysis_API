1712c42dcbaa146e86926a0d66dc01ddbd29b659
Shiska Raut <sraut@scellbio.com>
2024-08-21 12:34:36 -0500

    add urls and views

---
+++ assay_api/assay_api_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from assay_api_app import views
+
+urlpatterns = [
+    re_path(r"^$", views.landing, name="scellbio_landing_page"),
+    re_path(r"^cell_count_fluo", views.cell_count_fluo, name="cell_count_fluo"),
+    re_path(r"^cell_count_labelfree", views.cell_count_labelfree, name="cell_count_labelfree"),
+    re_path(r"^clono_assay", views.clono_assay, name="clono_assay"),
+    re_path(r"^clono_assay_labelfree", views.clono_assay_labelfree, name="clono_assay_labelfree"),
+]
--- assay_api/assay_api_app/views.py
+++ assay_api/assay_api_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def landing(request):
+    return render(request, "assay_api_app/scellbio_landing_page.html")
+
+def cell_count_fluo(request):
+    return render(request, "assay_api_app/cell_count_fluo.html")
+
+def cell_count_labelfree(request):
+    return render(request, "assay_api_app/cell_count_labelfree.html")
+
+def clono_assay(request):
+    return render(request, "assay_api_app/clono_assay.html")
+
+def clono_assay_labelfree(request):
+    return render(request, "assay_api_app/clono_assay_labelfree.html")
