ab47fb6aaddcb4227094231341621b9f02ee573c
Shiska Raut <sraut@scellbio.com>
2024-08-26 12:49:14 -0500

    remove background images

--- assay_api/static/scr/scellbio_landing.js
+++ assay_api/static/scr/scellbio_landing.js
@@ -1,7 +1,7 @@
-document.getElementById('assay_select').addEventListener('click', function() {
-        // Get the selected value from the dropdown
-        var selectedPage = document.getElementById('assaytype').value;
-
-        // Redirect the user to the selected page
-        window.location.href = selectedPage;
-    });
+// document.getElementById('id_assay_select').addEventListener('click', function() {
+//         // Get the selected value from the dropdown
+//         var selectedPage = document.getElementById('assaytype').value;
+//
+//         // Redirect the user to the selected page
+//         window.location.href = selectedPage;
+//     });
